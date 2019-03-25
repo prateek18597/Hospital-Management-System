9b84a5be5837986034715cb06f27de9f7237f01d
Pratik Parmar <prateek18597@gmail.com>
2019-03-25 16:51:37 +0530

    ShowBedRecords completed

--- src/ShowBedRecords.java
+++ src/ShowBedRecords.java
@@ -1,3 +1,11 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -63,12 +71,32 @@ public class ShowBedRecords extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTable1);
 
         jButton1.setText("Home");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Show");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Clear");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("Exit");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -125,6 +153,71 @@ public class ShowBedRecords extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        try
+        {
+            DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+            int rows = model.getRowCount();
+            if (rows > 0) {
+                for (int i = 0; i < rows; i++) {
+                    model.removeRow(0);
+                }
+            }
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/HMS_0049",Info.user,Info.pass);
+            Statement stat=conn.createStatement();
+            ResultSet rs=null;
+            
+            String query="Select BedNo,Bed.Room,concat(Patient.FirstName,' ',Patient.LastName),concat(Staff.FirstName,' ',Staff.LastName),Disease from Bed,Record,Patient,Staff,RecordDoctor";
+            query+= " where Bed.RecordId=Record.RecordId and Record.PatientId=Patient.Id and RecordDoctor.RecordId=Record.RecordId and Staff.Id=RecordDoctor.DoctorId;";
+            rs=stat.executeQuery(query);
+            while(rs.next())
+            {
+                String a1=rs.getString(1);
+                String a2=rs.getString(2);
+                String a3=rs.getString(3);
+                String a4=rs.getString(4);
+                String a5=rs.getString(5);
+                
+                model.addRow(new Object[] {a1,a2,a3,a4,a5});
+            }
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(rootPane, e.getMessage());
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
+            int rows = model.getRowCount();
+            if (rows > 0) {
+                for (int i = 0; i < rows; i++) {
+                    model.removeRow(0);
+                }
+            }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if(Info.Type.equals("Admin"))
+        {
+            this.dispose();
+            new LibHome().setVisible(true);
+        }
+        else
+        {
+            this.dispose();
+            new UserHome().setVisible(true);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
